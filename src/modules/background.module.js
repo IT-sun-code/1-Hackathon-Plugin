5cf3bc07fa6faea02da2a839068d8c3d43dad03f
IT-sun <orochimaro969@yandex.ru>
2022-12-10 17:19:55 +0300

    added html, css, background.module

--- src/modules/background.module.js
+++ src/modules/background.module.js
@@ -1,5 +1,14 @@
 import {Module} from '../core/module'
+import {getRandomColor} from '../utils'
 
 export class BackgroundModule extends Module {
-
+  async #getColor() {
+    document.body.style.backgroundColor = getRandomColor()
+    await new Promise(resolve => {
+      setTimeout(() => resolve(), 1000)
+    })
+  }
+  trigger(){
+    this.#getColor() 
+  } 
 }
