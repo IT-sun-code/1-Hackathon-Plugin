5691f74bc8310346f229fc75cc109e602fe60c6f
Julia Esutina <julia.esutina@gmail.com>
2022-12-10 15:53:37 +0300

    develop shape

--- src/modules/shape.module.js
+++ src/modules/shape.module.js
@@ -1,5 +1,59 @@
 import {Module} from '../core/module'
+import { random, getRandomColor } from '../utils'
 
 export class ShapeModule extends Module {
+  constructor() {
+    super('shape', 'Создать фигуру')
+    this.minSize = 100
+    this.maxSize = 500
+    this.screenWidth = document.body.clientWidth
+    this.screenHeight = document.body.clientHeight
+  }
 
+  #generateShape() {
+    const width = random(this.minSize, this.maxSize)
+    const height = random(this.minSize, this.maxSize)
+    const coordX = random(0,this.screenWidth - width)
+    const coordY = random(0,this.screenHeight - height)
+    const color = getRandomColor()
+    const borderRadius = random(0, 50)
+
+    return {
+      width,
+      height,
+      coordX,
+      coordY,
+      color,
+      borderRadius,
+    }
+  }
+
+  #draw(shapeParams) {
+    const div = document.createElement('div')
+    div.style.backgroundColor = shapeParams.color
+    div.style.width = `${shapeParams.width}px`
+    div.style.height = `${shapeParams.height}px`
+    div.style.position = 'absolute'
+    div.style.top = `${shapeParams.coordY}px`
+    div.style.left = `${shapeParams.coordX}px`
+    div.style.borderRadius = `${shapeParams.borderRadius}%`
+    document.body.append(div)
+    return div
+  }
+
+  async #remove(element, time) {
+    element.style.transition = "background-color 1s"
+    await new Promise(resolve => {
+      setTimeout(() => resolve(), time - 1000)
+    })
+    element.style.backgroundColor = "transparent"
+    await new Promise(resolve => setTimeout(() => resolve(), 1000)) 
+    element.remove()
+  }
+    
+  trigger() {
+    const shape = this.#generateShape()
+    const element = this.#draw(shape)
+    this.#remove(element, random(2000, 5000))
+  }
 }
