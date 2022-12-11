fe3a097b598dc7beea82516747b745be9681bc35
Zhserg28 <serg280880@gmail.com>
2022-12-11 20:34:33 +0800

    hotfix1

--- src/modules/clicks.module.js
+++ src/modules/clicks.module.js
@@ -3,8 +3,7 @@ import {Module} from '../core/module'
 export class ClicksModule extends Module {
     #countOneClick
     #countDoubleClick
-    #target
-    #init
+    #target    
     #timer
     constructor(type, text) {
         super(type, text)
@@ -15,13 +14,7 @@ export class ClicksModule extends Module {
     }
 
     async #getCountClick() {
-        // this.#target = true  
-        // this.#countOneClick = 0
-        // this.#countDoubleClick = 0
         
-        // this.#calcOneClick()
-        // this.#calcDblClick()
-
         const promise = new Promise(resolve => {         
             setTimeout(() => {                                   
                 resolve({cntOneClick: this.#countOneClick, cntDblClick: this.#countDoubleClick})
@@ -34,49 +27,56 @@ export class ClicksModule extends Module {
         alert(`за ${this.#timer/1000} сек. количество одиночных кликов - ${result.cntOneClick}, двойных кликов - ${result.cntDblClick}`)
         this.#countOneClick = 0
         this.#countDoubleClick = 0
-        console.log('after countOneClick ', this.#countOneClick)
-        console.log('after countDoubleClick ',this.#countDoubleClick)
+        
     }
 
-    #calcOneClick() {
-        const evt = document.body;
+    #calcOneClick(){
+        if (this.#target) this.#countOneClick+=1
+        console.log('countone ', this.#countOneClick)
+    }
+
+    #catchOneClick() {
+        //const evt = document.body;
         
-        // const func = function() {
-        //     if (this.#target) {
-        //         this.#countOneClick++
-        //     }
-        //      console.log('count after 1 ',count)
-        //     evt.removeEventListener("click", func.call())        
-        // }
-        
-        // evt.addEventListener("click", func.call())
+        document.addEventListener("click", this.#calcOneClick())
+
+        //evt.removeEventListener("click", this.#calcOneClick())
        
-        evt.addEventListener("click", () => {
-            if (this.#target) this.#countOneClick+=1
+        // evt.addEventListener("click", () => {
+        //     if (this.#target) this.#countOneClick+=1
            
             
-        })
-        evt.removeEventListener("click", () => {
-            if (this.#target) this.#countOneClick+=1        
+        // })
+        // evt.removeEventListener("click", () => {
+        //     if (this.#target) this.#countOneClick+=1        
             
-        })
+        // })
         
     }
 
-    #calcDblClick() {
-        const evt = document.body;
+    #calcDblClick(){
+        if (this.#target) this.#countDoubleClick+=1
+        if (this.#countOneClick>0) this.#countOneClick -= 2
+        console.log('countDouble ', this.#countDoubleClick)
+    }
 
+    #catchDblClick() {
+        //const evt = document.body;
 
-        evt.addEventListener("dblclick", () => {
-            if (this.#target) this.#countDoubleClick+=1
-            if (this.#countOneClick>0) this.#countOneClick -= 2
+        document.addEventListener("dblclick", this.#calcDblClick())
+
+        //evt.removeEventListener("dblclick", this.#calcDblClick())
+
+        // evt.addEventListener("dblclick", () => {
+        //     if (this.#target) this.#countDoubleClick+=1
+        //     if (this.#countOneClick>0) this.#countOneClick -= 2
             
-        })
-        evt.removeEventListener("dblclick", () => {
-            if (this.#target) this.#countDoubleClick+=1
-            if (this.#countOneClick>0) this.#countOneClick -= 2
+        // })
+        // evt.removeEventListener("dblclick", () => {
+        //     if (this.#target) this.#countDoubleClick+=1
+        //     if (this.#countOneClick>0) this.#countOneClick -= 2
             
-        })
+        // })
         //console.log('countDoubleClick ',this.#countDoubleClick)
         //console.log('event double',event)
     }
@@ -86,8 +86,8 @@ export class ClicksModule extends Module {
         this.#countOneClick = 0
         this.#countDoubleClick = 0
         
-        this.#calcOneClick()
-        this.#calcDblClick()      
+        this.#catchOneClick()
+        this.#catchDblClick()      
         this.#getCountClick()       
     }
 }
